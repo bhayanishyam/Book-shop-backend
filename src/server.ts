accff099f10e98e14b9f4819f255dfa810dbd3e8
shyam <bhayanishyam77@gmail.com>
2024-12-30 15:49:45 +0530

    project setup

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
+app.listen(port, () => {
+    console.log(`Sever listen to Post ${port}`);
+})
+
