04530836cc3b5a99e64c3b9c480fe9ae622af40c
shyam <bhayanishyam77@gmail.com>
2025-01-01 13:16:37 +0530

    solve the CORS error and add logger

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,24 @@ import connect from './config/dbconfig'
 import userRoute from './routes/userRoute'
 import categoryRoute from './routes/categoryRoute'
 import { errorHandler } from './middleware/errorHandler'
+import morgan from 'morgan'
+import cors from 'cors'
+
 dotenv.config()
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 
 connect()
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors(corsOptions))
 app.use(express.json())
+app.use(morgan('tiny'))
 
 // testing api
 app.get("/", (req, res) => {
@@ -20,6 +31,10 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRoute)
 app.use("/api/category", categoryRoute)
 app.use(errorHandler)
+
+app.use("/*", (req, res) => {
+    res.status(404).json({ message: "Not Found" })
+})
 
 
 app.listen(port, () => {
